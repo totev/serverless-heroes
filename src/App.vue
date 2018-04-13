b468bc0cdd5ecdcc7ee4d5bf32a8eb360e666d16
Dobroslav Totev <dev@totev.de>
2018-04-13 14:56:37 +0200

    (feature): added some marvel api functionality

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app">
+        <RandomHero></RandomHero>
+    </div>
+</template>
+
+<script>
+    import RandomHero from './components/RandomHero.vue'
+
+    export default {
+        name: 'app',
+        components: {
+            RandomHero
+        }
+    }
+</script>
+
+<style>
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
---
+++ src/components/RandomHero.vue
@@ -0,0 +1,81 @@
+<template>
+    <div v-if="hero">
+        <div class="random-hero">
+            <div class="about">
+                <div class="bold">{{hero.name}}</div>
+                <div class="description">{{hero.description}}</div>
+            </div>
+            <div class="portrait">
+                <img :src="portrait"/>
+            </div>
+        </div>
+
+        <footer>Copyright api and api-response: {{ copyright }}</footer>
+    </div>
+
+</template>
+
+<script>
+    import axios from "axios";
+    import first from "lodash/first";
+    const APIKEY = process.env.VUE_APP_MARVEL_API_KEY
+
+    export default {
+        name: "RandomHero",
+        data () {
+            return {
+                hero: undefined,
+                copyright: "© 2018 MARVEL",
+                portrait: ""
+            };
+        },
+        async created () {
+            const heroesRequest = await  axios.get("http://gateway.marvel.com/v1/public/characters/1009351", {
+                params: {apikey: APIKEY}
+            });
+            const remoteHero = heroesRequest.data.data
+            this.hero = first(remoteHero.results)
+            console.log("baml!", JSON.stringify(this.hero));
+            this.portrait = `${this.hero.thumbnail.path}/portrait_uncanny.${this.hero.thumbnail.extension}`
+
+        },
+        methods: {},
+    }
+</script>
+
+<style scoped>
+    .bold{
+        font-weight: bold;
+    }
+
+    footer {
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        padding: 1rem;
+        background-color: #efeeea;
+        text-align: center;
+    }
+
+    .random-hero {
+        width: 100%;
+        heght: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .about{
+        padding: 1em;
+    }
+
+    .about .description{
+        text-align: justify;
+    }
+
+    .portrait{
+        padding: 1em;
+    }
+
+</style>
